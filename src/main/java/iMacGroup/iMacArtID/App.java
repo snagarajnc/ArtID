6dff652741eedf6d87efb180ade32df8578b1298
snagarajnc <snagarajnc@gmail.com>
2019-02-26 14:43:43 +0530

    # WARNING: head commit changed in the meantime
    Added Ignore

---
+++ src/main/java/iMacGroup/iMacArtID/App.java
@@ -0,0 +1,14 @@
+package iMacGroup.iMacArtID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Output Changed" );
+    }
+}
